a61b281b46ea4bab9ed535f44fc0c35d2dabd2f8
TheJeffi <TheJeffI@yandex.ru>
2022-06-22 01:20:22 +0300

    add namespace

--- Tuple/Program.cs
+++ Tuple/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Tuple
+﻿namespace LearnCSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] names = new string[] { "Tom", "Bob", "Sam" };
+
+            (int id, string name)[] users = new (int, string)[3];
+            for (int i = 0; i < names.Length; i++)
+            {
+                users[i] = (i, names[i]);
+            }
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine($"ID: {users[i].id}, Name: {users[i].name}");
+            }
+
+            var someTuple = (1, "Lorem");
+
+            GetValue(someTuple);
+        }
+
+        public static void GetValue((int Item1, string Item2) tuple)
+        {
+            Console.WriteLine($"Tuple first item = {tuple.Item1} type: {tuple.Item1.GetType()}, second item = {tuple.Item2}, type {tuple.Item2.GetType()}");
         }
     }
 }
+
