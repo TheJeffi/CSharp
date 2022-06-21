aa0e0e6b55a68769aadcdb113b159e8c04eae4da
TheJeffi <TheJeffI@yandex.ru>
2022-06-22 01:07:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace LearnCSharp
+{
+    public static class Extensions
+    {
+        public static int ExtendedAverage(this int[] num)
+        {
+            int average = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                average += num[i];
+            }
+            average /= num.Length;
+            return average;
+        }
+
+        public static int ExtendedCharCount(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
+        }
+
+        public static string ExtendedToString(this int num)
+        {
+            num += num;
+            return num.ToString();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5] { 1, 2, 3, 442, 5 };
+
+            string newNum = numbers[3].ExtendedToString();
+
+            Console.WriteLine($"{numbers.ExtendedAverage()}, {newNum}, {newNum.ExtendedCharCount('8')}");
+        }
+    }
+}
