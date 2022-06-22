d61d3cd247693094c2ca4ad4d5c0b1956989923b
TheJeffi <TheJeffI@yandex.ru>
2022-06-22 15:40:57 +0300

    codeclean

--- DB/Program.cs
+++ DB/Program.cs
@@ -21,32 +21,29 @@ namespace DB
 
         private void AddPersons()
         {
-            using (ApplicationContext db = new ApplicationContext())
+            using ApplicationContext db = new();
+            ConsoleKey consoleKey = ConsoleKey.Y;
+            while (consoleKey == ConsoleKey.Y)
             {
-                ConsoleKey consoleKey = ConsoleKey.Y;
-                while (consoleKey == ConsoleKey.Y)
-                {
-                    User user = new User();
+                User user = new User();
 
-                    Console.WriteLine("Write name");
-                    user.FirstName = Console.ReadLine();
+                Console.WriteLine("Write name");
+                user.FirstName = Console.ReadLine();
 
-                    Console.WriteLine("Write lastname");
-                    user.LastName = Console.ReadLine();
+                Console.WriteLine("Write lastname");
+                user.LastName = Console.ReadLine();
 
-                    Console.WriteLine("Write Birthday (dd.mm.yyyy)");
-                    user.Birthday = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Write Birthday (dd.mm.yyyy)");
+                user.Birthday = DateTime.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Write gender (m/f)");
-                    user.Gender = char.Parse(Console.ReadLine());
+                Console.WriteLine("Write gender (m/f)");
+                user.Gender = char.Parse(Console.ReadLine());
 
-                    db.Users.Add(user);
-                    db.SaveChanges();
+                db.Users.Add(user);
+                db.SaveChanges();
 
-                    Console.WriteLine("Add new person? Y - yes, any button - no");
-                    consoleKey = Console.ReadKey(true).Key;
-                }
-
+                Console.WriteLine("Add new person? Y - yes, any button - no");
+                consoleKey = Console.ReadKey(true).Key;
             }
         }
         private void DeleteFromDB()
@@ -55,22 +52,20 @@ namespace DB
             string name = Console.ReadLine();
 
             string sqlExpression = $"DELETE From Users WHERE FirstName = '{name}'";
-            using (var connection = new SqliteConnection("Data Source=persons.db"))
+            using var connection = new SqliteConnection("Data Source=persons.db");
+            connection.Open();
+
+            try
             {
-                connection.Open();
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                int number = command.ExecuteNonQuery();
 
-                try
-                {
-                    SqliteCommand command = new SqliteCommand(sqlExpression, connection);
-                    int number = command.ExecuteNonQuery();
+                Console.WriteLine($"Deleted objects: {number}");
+            }
+            catch (Exception)
+            {
 
-                    Console.WriteLine($"Deleted objects: {number}");
-                }
-                catch (Exception)
-                {
-
-                    throw new Exception("Error when working with SQLite");
-                }
+                throw new Exception("Error when working with SQLite");
             }
         }
 
@@ -83,29 +78,33 @@ namespace DB
                 int id = int.Parse(Console.ReadLine());
 
                 string sqlExpression = $"DELETE FROM Users WHERE id={id}";
-                using (var connection = new SqliteConnection("Data Source=persons.db"))
-                {
-                    connection.Open();
+                using var connection = new SqliteConnection("Data Source=persons.db");
+                connection.Open();
 
+                try
+                {
                     SqliteCommand command = new SqliteCommand(sqlExpression, connection);
 
                     int number = command.ExecuteNonQuery();
 
                     Console.WriteLine($"Deleted objects: {number}");
                 }
+                catch (Exception)
+                {
+
+                    throw new Exception("Error when working with SQLite");
+                }
             }
         }
 
         private void PrintPersons()
         {
-            using (ApplicationContext db = new ApplicationContext())
-            {
-                var users = db.Users.ToList();
+            using ApplicationContext db = new();
+            var users = db.Users.ToList();
 
-                foreach (User user in users)
-                {
-                    Console.WriteLine($"ID: {user.Id} Fullname:  {user.FirstName} {user.LastName} Birthday: {user.Birthday} Gender: {user.Gender}");
-                }
+            foreach (User user in users)
+            {
+                Console.WriteLine($"ID: {user.Id} Fullname:  {user.FirstName} {user.LastName} Birthday: {user.Birthday} Gender: {user.Gender}");
             }
         }
     }
