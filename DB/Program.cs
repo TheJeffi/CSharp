63c2ea29390046d1ab2155b6a0b4eda433491e14
TheJeffi <TheJeffI@yandex.ru>
2022-06-22 14:38:38 +0300

    Add DB logic

--- DB/Program.cs
+++ DB/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Microsoft.Data.Sqlite;
 
 namespace DB
 {
@@ -6,20 +6,98 @@ namespace DB
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+/*            program.AddPerson();
+            program.AddPerson();*/
+
+            program.RemovePerson();
+
+            program.PrintPersons();
+
+            program.DeleteFromDB();
+
+            program.PrintPersons();
+        }
+
+        private void AddPerson()
+        {
             using (ApplicationContext db = new ApplicationContext())
             {
+                User user = new User();
 
-                User tom = new User { FirstName = "Tom", LastName = "Jackson", Birthday = DateTime.Parse("10.01.2021"), Gender = 'm' };
-                User alice = new User { FirstName = "Alice", LastName = "Thompson", Birthday = DateTime.Parse("10.04.2008"), Gender = 'f' };
+                Console.WriteLine("Write name");
+                user.FirstName = Console.ReadLine();
 
-                db.Users.Add(tom);
-                db.Users.Add(alice);
-                User Jack = new User { FirstName = "Jack", LastName = "Jackson", Birthday = DateTime.Parse("10.01.2021"), Gender = 'm' };
-                db.Users.Add(Jack);
+                Console.WriteLine("Write lastname");
+                user.LastName = Console.ReadLine();
+
+                Console.WriteLine("Write Birthday (dd.mm.yyyy)");
+                user.Birthday = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Write gender (m/f)");
+                user.Gender = char.Parse(Console.ReadLine());
+
+                db.Users.Add(user);
                 db.SaveChanges();
-                Console.WriteLine("DB save");
+            }
+        }
+
+        private void DeleteFromDB()
+        {
+            Console.WriteLine("Enter the name of the person you want to delete");
+            string name = Console.ReadLine();
+
+            string sqlExpression = $"DELETE From Users WHERE FirstName = '{name}'";
+            using (var connection = new SqliteConnection("Data Source=persons.db"))
+            {
+                connection.Open();
+
+                try
+                {
+                    SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                    int number = command.ExecuteNonQuery();
+
+                    Console.WriteLine($"Deleted objects: {number}");
+                }
+                catch (Exception)
+                {
+
+                    throw new Exception("Error when working with SQLite");
+                }
+                
+                
+            }
+        }
+        
+        private void RemovePerson()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Console.WriteLine("select id to delete");
+
+                int id = int.Parse(Console.ReadLine());
+
+                string sqlExpression = $"DELETE FROM Users WHERE id={id}";
+                using (var connection = new SqliteConnection("Data Source=persons.db"))
+                {
+                    connection.Open();
+
+                    SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                    int number = command.ExecuteNonQuery();
+
+                    Console.WriteLine($"Deleted objects: {number}");
+                }
+            }
+        }
+
+        private void PrintPersons()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
                 var users = db.Users.ToList();
-                Console.WriteLine("Users:");
+
                 foreach (User user in users)
                 {
                     Console.WriteLine($"ID: {user.Id} Fullname:  {user.FirstName} {user.LastName} Birthday: {user.Birthday} Gender: {user.Gender}");
